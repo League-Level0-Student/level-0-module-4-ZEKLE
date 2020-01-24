30b7f34110f3835e0536d4f13f4c319c06733a33
League Student <league@WTS14-3.attlocal.net>
2020-01-23 16:58:35 -0800

    week 21

--- src/_07_sleepy_head/SleepyHead.java
+++ src/_07_sleepy_head/SleepyHead.java
@@ -18,13 +18,30 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
-
-
+         //JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+         String number = JOptionPane.showInputDialog(null, "is it a weekday?");
+         
+         if(number.equals("yes")) {
+        	 String vacation = JOptionPane.showInputDialog(null, "are we on vaction?");
+        	 if(vacation.equals("yes")) {
+        		 JOptionPane.showMessageDialog(null, "Get up were on vacation!");
+        	 } else {
+        	 if(vacation.equals("no"));
+        	 JOptionPane.showMessageDialog(null, "GET UP YOUR LATE FOR SCHOOL!");
+         }
+         if(number.equals("no")) {
+        	 JOptionPane.showMessageDialog(null, "sleep in");
+         }
+         }
         /*
          * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!�? If it is a weekday, and we are on vacation,
          * print “sleep in�?.
          */
     }
+
+	private static void showInputDialog(Object object, String string) {
+		// TODO Auto-generated method stub
+		
+	}
 }
